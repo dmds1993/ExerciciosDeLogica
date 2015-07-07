a9551a6886bc037a79a2e908f7d0fcbbcdfbf946
Daniel Santos <daniel.santos@arizona.com.br>
2015-07-07 00:14:43 -0300

    Exercicios de 1 á 3

---
+++ ExerciciosDeLogica/src/br/com/logica/de/programacao/estrutura/NomeEendereco.java
@@ -0,0 +1,15 @@
+package br.com.logica.de.programacao.estrutura;
+
+public class NomeEendereco {
+
+	public static void main(String[] args) {
+		String nomeCompleto = "Daniel D M S";
+		String endereco = "Avenida Morumbi";
+		String cep = "05864-370";
+		String bairro = "Jardim Maria Virginia";
+		
+		
+		System.out.println ("Nome completo: "+nomeCompleto+"\nEndereço:"+
+		endereco+"\ncep: "+cep+"\nBairro: "+bairro);
+	}
+}
