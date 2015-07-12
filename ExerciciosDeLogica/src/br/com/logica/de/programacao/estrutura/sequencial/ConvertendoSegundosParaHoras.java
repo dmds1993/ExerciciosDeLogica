008481ab52c7d5f7c027d829596236db5600e8df
Daniel Santos <daniel.santos@arizona.com.br>
2015-07-12 16:50:41 -0300

    Exercicio 16

--- ExerciciosDeLogica/src/br/com/logica/de/programacao/estrutura/sequencial/ConvertendoSegundosParaHoras.java
+++ ExerciciosDeLogica/src/br/com/logica/de/programacao/estrutura/sequencial/ConvertendoSegundosParaHoras.java
@@ -13,13 +13,42 @@ public class ConvertendoSegundosParaHoras {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int segundos = 0;
-		int minuto = 60;
-		int umaHora = minuto * 60;
+		double minutos = 60;
+		double horas = 3600;
 		System.out.println ("Digite os seguntos para saber as horas");
 		segundos = scanner.nextInt();
 	
+		double horasConvertidas = (segundos / horas);
+		int inteiroHoras;
+		double restoHoras;
+		double multiplicandoPraTerOsMinutos;
+		double restoSegundos;
+		inteiroHoras = (int) (horasConvertidas); 
+		//Aqui é pego apenas as horas.
 		
-		System.out.println (horasConvertidas);
+		restoHoras = horasConvertidas - inteiroHoras;
+		multiplicandoPraTerOsMinutos = restoHoras * 60; 
+		//Aqui é convertido o resto para minutos.
+		
+		int inteiroDosMinutos;
+		inteiroDosMinutos = (int) (multiplicandoPraTerOsMinutos);
+		//Aqui é retirado o resto da divisão para ter apenas os minutos.
+		
+		int inteiroSegundos;
+		inteiroSegundos = (int) (multiplicandoPraTerOsMinutos);
+		restoSegundos = multiplicandoPraTerOsMinutos - inteiroSegundos;
+		//Aqui é retirado o resto da divisão dos minutos
+		double convertendoSegundos;
+		convertendoSegundos = restoSegundos * 60;
+		//Aqui se converte para segundos
+		int segundosConvertidos;
+		segundosConvertidos = (int) (convertendoSegundos);
+		//Aqui é retirado os resto da divisão mantendo apenas os segundos.
+		
+		System.out.println (inteiroHoras+"h:"+":"+inteiroDosMinutos+"m:"+segundosConvertidos+"s");
+		// Aqui foi formatado para a impressão de 00h: 00m: 00s:
+		
+		
 	}
 
 }
