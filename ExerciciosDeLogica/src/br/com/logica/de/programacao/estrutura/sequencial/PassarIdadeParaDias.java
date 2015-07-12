bfb08fb3d48b00ddc4128198e0a0d2432b46763f
Daniel Santos <daniel.santos@arizona.com.br>
2015-07-12 01:47:27 -0300

    Exercicio 12

--- ExerciciosDeLogica/src/br/com/logica/de/programacao/estrutura/sequencial/PassarIdadeParaDias.java
+++ ExerciciosDeLogica/src/br/com/logica/de/programacao/estrutura/sequencial/PassarIdadeParaDias.java
@@ -1,6 +1,10 @@
-package br.com.logica.de.programacao.estrutura;
+package br.com.logica.de.programacao.estrutura.sequencial;
 
 import java.util.Scanner;
+
+/*12. Faça um programa que leia a idade de uma pessoa em ano, calcule e  mostre a idade correspondente ao
+ * total de dias vividos por essa pessoa, considerando que todos os meses são de 30 dias.
+ */
 
 public class PassarIdadeParaDias {
 
