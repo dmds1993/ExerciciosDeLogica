4f1a84edf06631e33d8c53d2ca168e1d6ba5cf61
Daniel Santos <daniel.santos@arizona.com.br>
2015-07-13 22:49:59 -0300

    Exercicio 22

--- ExerciciosDeLogica/src/br/com/logica/de/programacao/estrutura/de/decisao/RecebendoValores.java
+++ ExerciciosDeLogica/src/br/com/logica/de/programacao/estrutura/de/decisao/RecebendoValores.java
@@ -17,7 +17,38 @@ public class RecebendoValores {
 		b = scanner.nextInt();
 		System.out.println ("Digite o valor B.");
 		c = scanner.nextInt();
-	
+		int operacao = 0;
+		switch (operacao) {
+		case 1:
+			b = a;
+			System.out.println ("O valor de B é igual a A, favor inserir o valor de B novamente");
+			b = scanner.nextInt();
+			break;
+			
+		case 2:
+			c = a;
+			System.out.println("O valor de C é igual ao valor de A, favor inserir o valor de C novamente:");
+			c = scanner.nextInt();
+			break;
+		case 3:
+			c = b;
+			System.out.println("O valor de C é igual ao valor de B, favor inserir o valor de C novamente");
+			c = scanner.nextInt();
+			break;
+			
+		default:
+			
+		}
+		int operacao2 = 0;
+		switch (operacao2) {
+		case 1:
+			
+			break;
+
+		default:
+			break;
+		}
+		
 	}
 	
 }
