bfe18d01555b06fddb39114b3e5604c3aa26a17f
Daniel Santos <daniel.santos@arizona.com.br>
2015-07-26 02:03:37 -0300

    Exercicio 27

---
+++ ExerciciosDeLogica/src/br/com/logica/de/programacao/estacionamento/ValorPorHora.java
@@ -0,0 +1,11 @@
+package br.com.logica.de.programacao.estacionamento;
+
+import java.math.BigDecimal;
+
+public class ValorPorHora {
+
+	BigDecimal valorHora = new BigDecimal("8.00");
+	BigDecimal valorDuasHoras = new BigDecimal("15.00");
+	BigDecimal valorTresHoras = new BigDecimal ("5.00");
+	
+}
